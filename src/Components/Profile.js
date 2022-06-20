7a6c20b64f27084b121129dc18870bb4b4298796
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-20 11:01:00 +0530

    Basic application setup complete in react-redux

---
+++ src/Components/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Hello, This is the profile page.</h1>
+      <ul>
+        <li>Name: </li>
+        <li>Age: </li>
+        <li>Email: </li>
+        <li>Phone Number: </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
